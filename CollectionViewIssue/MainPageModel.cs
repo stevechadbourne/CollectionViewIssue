9f57aa6deee8c7aee950ed2e3d552b588594279a
Steve Chadbourne <steve.chadbourne@univerus.com>
2024-06-24 09:25:13 +1200

    Add project files.

---
+++ CollectionViewIssue/MainPageModel.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CollectionViewIssue
+{
+    public class MainPageModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<string> Numbers { get; set; } = ["One", "Two", "Three"];
+
+        private string _selectedNumber;
+        public string SelectedNumber
+        {
+            get => _selectedNumber;
+            set
+            {
+                BottomNumber = value;
+                _selectedNumber = null;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _bottomNumber;
+        public string BottomNumber
+        {
+            get => _bottomNumber;
+            set
+            {
+                _bottomNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ CollectionViewIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CollectionViewIssue
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
